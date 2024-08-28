43108c503d932a61de816921627bacf6f20f3b71
JBevc <141636441+JBevc@users.noreply.github.com>
2024-08-28 18:20:42 -0300

    agregue menu

--- script.js
+++ script.js
@@ -1,4 +1,78 @@
-document.addEventListener("DOMContentLoaded", () => {
+let tasks = [];
+
+function createTaskComponent(taskElement) {
+  const templateContent = `
+    <div class="card cardTask js-modal-trigger" data-target="modal-js-example">
+      <div class="media-content">
+        <span class="tag ${taskElement.tagColor}"> </span>
+        <p class="title is-6"> ${taskElement.tituloTask} </p>
+      </div>
+      <div class="content">
+        <p> ${taskElement.descripcionTask} </p>
+      </div>
+    </div>
+  `;
+
+  const box = document.getElementById(
+    String(taskElement.estadoTask).toLowerCase()
+  );
+  const element = box.querySelector(".buttonBox");
+  console.log("box", box);
+  const elementoAgregar = document.createElement("div");
+  elementoAgregar.innerHTML = templateContent;
+  box.insertBefore(elementoAgregar, element);
+}
+function loadTasks(taskList) {
+  taskList.forEach((element) => {
+    createTaskComponent(element);
+  });
+}
+
+function addTaskHandler() {
+  // ignora evento reset de submit button
+  //event.preventDefault(true);
+
+  const tituloInput = document.getElementById("titulo").value.trim();
+  const descInput = document.getElementById("descripcion").value.trim();
+  const asignadoInput = document.getElementById("asignado").value.trim();
+  const prioridadInput = document.getElementById("prioridad").value.trim();
+  const estadoInput = document.getElementById("estado").value.trim();
+  const fechaInput = document.getElementById("fechaLimite").value.trim();
+
+  if (prioridadInput == "Alta") {
+    color = "is-danger";
+  }
+  if (prioridadInput == "Media") {
+    color = "is-warning";
+  }
+  if (prioridadInput == "Baja") {
+    color = "is-success";
+  }
+
+  const id = tasks.length + 1;
+  const tareaNueva = {
+    idTask: id,
+    estadoTask: estadoInput,
+    descripcionTask: descInput,
+    tituloTask: tituloInput,
+    asignadoTask: asignadoInput,
+    prioridadTask: prioridadInput,
+    tagColor: color,
+    fechaLimiteTask: fechaInput,
+  };
+  tasks.push(tareaNueva);
+  createTaskComponent(tareaNueva);
+}
+// para resetear el texto del form
+//document.querySelector(".main__form").reset();
+
+const aceptar = document.getElementById("addTaskButton");
+aceptar.addEventListener("click", () => {
+  addTaskHandler();
+  loadModalEvents();
+});
+
+function loadModalEvents() {
   // Functions to open and close a modal
   function openModal($el) {
     $el.classList.add("is-active");
@@ -43,4 +117,6 @@ document.addEventListener("DOMContentLoaded", () => {
       closeAllModals();
     }
   });
-});
+}
+
+loadModalEvents();
